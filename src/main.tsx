b7fdc19533564971adce61e461f79bbaffa62247
Atharv shrivastava <shashishrivastava0290@gmail.com>
2026-09-16 18:38:03 +0530

    feat: mount bidirectional communications bridge

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import 'chart.js/auto'
 import './index.css'
 import App from './App'
+import CommunicationsBridge from './CommunicationsBridge'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <CommunicationsBridge />
   </React.StrictMode>
 )
